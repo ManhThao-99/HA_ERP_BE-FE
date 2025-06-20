8a4be0e18139e774057558a33bd61484b32596ba
ThaoBB <ThaoBB>
2025-06-20 14:14:44 +0700

    Initial commit

---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'HA_ERP',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44335/',
+    redirectUri: baseUrl,
+    clientId: 'HA_ERP_App',
+    responseType: 'code',
+    scope: 'offline_access HA_ERP',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44335',
+      rootNamespace: 'HA_ERP',
+    },
+  },
+} as Environment;
